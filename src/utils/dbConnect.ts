8861c3d8c00c6a21529e46c4b3dbf332e56e1ad3
Utkarsh-Anandani <utkarshanandani9@gmail.com>
2025-10-03 16:36:07 +0530

    Added logs to dbConnect

--- src/utils/dbConnect.ts
+++ src/utils/dbConnect.ts
@@ -1,14 +1,22 @@
 import mongoose from 'mongoose';
-import 'dotenv/config'
+import 'dotenv/config';
 
 const MONGODB_URI = process.env.MONGODB_URI!;
 
 async function dbConnect() {
   if (mongoose.connection.readyState >= 1) {
+    console.log('Already connected to MongoDB.');
     return;
   }
 
-  await mongoose.connect(MONGODB_URI);
+  console.log('Attempting to connect to MongoDB...');
+  
+  try {
+    await mongoose.connect(MONGODB_URI);
+    console.log('Successfully connected to MongoDB.');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+  }
 }
 
 export default dbConnect;
